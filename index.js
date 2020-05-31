75f15680c570407a9150e0690d360c3a3a4603a4
Jesus <jpalaciosmendias0@gmail.com>
2020-05-30 20:19:44 -0600

    Jesus Emanuel Palacios Mendias 4-P
    actualización de datos de la pagina.

--- index.js
+++ index.js
@@ -4,10 +4,49 @@ const hbs = require('hbs');
 const app = express();
 
 app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',  (req, res)=>{
-    res.render('index');
+    res.render('index', {
+        autor :"Jesus Palacios",
+        year : "2020",
+        title : "Inicio"
+    });
+});
+
+app.get('/picaso',  (req, res)=>{
+    res.render('picaso', {
+        autor :"Jesus Palacios",
+        year : "2020",
+        title : "Pablo Picasso"
+    });
+});
+
+app.get('/gogh',  (req, res)=>{
+    res.render('gogh', {
+        autor :"Jesus Palacios",
+        year : "2020",
+        title : "Van Gogh"
+    });
+});
+
+app.get('/jean',  (req, res)=>{
+    res.render('jean', {
+        autor :"Jesus Palacios",
+        year : "2020",
+        title : "Jean"
+    });
+});
+
+app.get('/leonardo',  (req, res)=>{
+    res.render('leonardo', {
+        autor :"Jesus Palacios",
+        year : "2020",
+        title : "Leonardo da Vinci"
+    });
 });
 
 app.listen(3000, ()=>{
